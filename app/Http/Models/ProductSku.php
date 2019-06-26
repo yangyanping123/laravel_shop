769191d5992ad1731fec7c8a41890f84794dbf74
root <root@localhost.localdomain>
2019-06-26 16:46:24 +0800

    管理后台 - 商品列表

---
+++ app/Http/Models/ProductSku.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
