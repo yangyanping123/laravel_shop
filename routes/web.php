155d371f1b76213186c15d7cbfeae824e1949409
yyp <1306701423@qq.com>
2019-07-04 18:01:56 +0800

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,11 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+    //支付宝
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
@@ -50,3 +55,5 @@ Route::get('products/{product}', 'ProductsController@show')->name('products.show
         'subject' => 'test subject - 测试',
     ]);
 });*/
+
+Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
