2d9faa1c8ef2cda189b87516c9c91283f69a819c
yyp <1306701423@qq.com>
2019-07-04 14:51:36 +0800

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 use App\Http\Models\CartItem;
+use App\Http\Models\ProductSku;
 class CartController extends Controller
 {
+    //添加购物车
     public function add(AddCartRequest $request)
     {
         $user   = $request->user();
@@ -28,6 +30,21 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+
+    //查看购物车
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+  //移除购物车
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
