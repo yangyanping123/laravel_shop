fc6c70733d02e54ddd519ef3caab96c990736950
yyp <1306701423@qq.com>
2019-07-09 18:15:52 +0800

    update readme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+/**
+ * 实例化application
+ */
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
@@ -26,16 +19,19 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 
+//http kernel单例
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
 );
 
+//concole kernel单例
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
 );
 
+//异常处理单例
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
@@ -51,5 +47,5 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-
+ //返回app
 return $app;
--- readme.txt
+++ readme.txt
@@ -9,9 +9,20 @@ https://cloud.tencent.com/developer/article/1360210
 composer国内镜像
 composer config -g repos.packagist composer https://packagist.mirrors.sjtug.sjtu.edu.cn
 composer config -g repos.packagist composer https://php.cnpkg.org
-composer config -g repo.packagist composer https://packagist.laravel-china.org
+composer config -g repos.packagist composer https://mirrors.cloud.tencent.com/composer/
 
 
 Laravel5.6 整合 RabbitMQ 消息队列
 https://learnku.com/articles/21522
 
+laravel底层分析生命周期和容器 Container
+https://learnku.com/laravel/t/29566#d64937
+https://learnku.com/laravel/t/29612  生命周期——启动框架
+https://learnku.com/laravel/t/29922 生命周期——路由器
+https://learnku.com/laravel/t/30061 生命周期——响应（
+
+
+https://learnku.com/laravel/t/2093/laravel-source-analysis-series-cache 底层分析 cache
+
+https://learnku.com/articles/30822  Redis 应用-限流
+https://learnku.com/articles/30329  持续更新免费的 API，做一个 API 的搬运工——终身维护
