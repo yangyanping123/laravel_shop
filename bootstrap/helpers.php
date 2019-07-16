0a1e3298b7a3272d28a4b4ad5193a727433582b7
yyp <1306701423@qq.com>
2019-07-16 23:28:41 +0800

    开发环境加入 Ngrok

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -18,3 +18,15 @@ function big_number($number, $scale = 2)
 {
     return new \Moontoast\Math\BigNumber($number, $scale);
 }
+
+//ngrok测试函数
+function ngrok_url($routeName, $parameters = [])
+{
+    // 开发环境，并且配置了 NGROK_URL
+    if(app()->environment('local') && $url = config('app.ngrok_url')) {
+        // route() 函数第三个参数代表是否绝对路径
+        return $url.route($routeName, $parameters, false);
+    }
+
+    return route($routeName, $parameters);
+}
